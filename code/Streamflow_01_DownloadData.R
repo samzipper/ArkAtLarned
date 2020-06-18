4d1289d1c3fc56367d7c4dd1172cdfef8b911687
samzipper <samuelczipper@gmail.com>
2020-06-18 10:44:00 -0500

    Add some aesthetic info to paths+packages

--- code/Streamflow_01_DownloadData.R
+++ code/Streamflow_01_DownloadData.R
@@ -6,8 +6,9 @@ source(file.path("code", "paths+packages.R"))
 ##Grab data from Arkansas River
 USGS_id <- "07141220" #The Arkansas River at Larned 
 
-df_site <- dataRetrieval::readNWISdv(USGS_id, "00060",
-                                     "","") %>%
+df_site <- 
+  dataRetrieval::readNWISdv(USGS_id, "00060",
+                            "","") %>%
   dplyr::select(site_no, Date, X_00060_00003) %>% 
   magrittr::set_colnames(c("gageid", "Date", "discharge_cfs")) %>% 
   dplyr::mutate(discharge_cms = discharge_cfs*(0.3048^3),
--- code/paths+packages.R
+++ code/paths+packages.R
@@ -3,11 +3,34 @@
 #load packages
 library(tidyverse)
 library(lubridate)
+library(magrittr)
 library(dataRetrieval)
 library(EcoHydRology)
 
+## color palettes
+# categorical color palette from https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
+col.cat.grn <- "#3cb44b"   # green
+col.cat.yel <- "#ffe119"   # yellow
+col.cat.org <- "#f58231"   # orange
+col.cat.red <- "#e6194b"   # red
+col.cat.blu <- "#0082c8"   # blue
+col.gray <- "gray65"       # gray for annotation lines, etc
 
+## ggplot theme
+windowsFonts(Arial=windowsFont("TT Arial"))
+theme_scz <- function(...){
+  theme_bw(base_size=10, base_family="Arial") + 
+    theme(
+      text=element_text(color="black"),
+      plot.title=element_text(face="bold", size=rel(1)),
+      axis.title=element_text(face="bold", size=rel(1)),
+      axis.text=element_text(size=rel(1)),
+      strip.text=element_text(size=rel(1)),
+      legend.title=element_text(face="bold", size=rel(1)),
+      legend.text=element_text(size=rel(1)),
+      panel.grid=element_blank(),
+      plot.margin=unit(c(1,1,1,1), "mm"),
+      strip.background=element_blank())
+}
 
-##path to directory for project
-path_repo <- file.path("C:/Users", "kylec", "Documents", "ArkAtLarned")
-
+theme_set(theme_scz())
