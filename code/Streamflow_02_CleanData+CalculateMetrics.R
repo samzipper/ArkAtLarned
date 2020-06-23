a8dfd34d5bcbe7ef79140063aa55bd8de19a5648
kylecomp <kac15c@my.fsu.edu>
2020-06-22 20:26:12 -0500

    Adding more hydrologic Stats

--- code/Streamflow_02_CleanData+CalculateMetrics.R
+++ code/Streamflow_02_CleanData+CalculateMetrics.R
@@ -15,29 +15,64 @@ data_path<- file.path("data", "Historical_Larned_Arkflow.CSV")
     #Group Data by Month
     stream_monthly<-
       stream_df %>%
-      dplyr::group_by(Year, Month)
+      dplyr::group_by(year, month) %>%
       dplyr::summarize(Date_mid = mean(Date),
                        Q_cms_mean = mean(discharge_cms))
-  } 
-
+    
+    #Calculate frequency of No Flow
+    #Group by year
+    stream_df<-
+      stream_df %>%
+      dplyr::mutate(DOY = lubridate::yday(lubridate::ymd(Date))) %>%
+      dplyr::group_by(year)
+   
+    
+     dry_df<-stream_df%>%
+       filter(discharge_cms==0)
+    
+    
+    y=count(dry_df, year)
+    year<-c(as.numeric(first(stream_df$year):last(stream_df$year)))
+    n<-c(1:length(year))*0
+    days<-as.numeric(lubridate::leap_year(year))+365 
+  
+    
+   DryDays<-data.frame(year, n, days)
+    
+   DryDays2<-merge(DryDays, y, by.x = 'year', by.y = 'year', all.x = TRUE) 
+   DryDays2[is.na(DryDays2)]<-0
+   DryDays2$n.x<-DryDays2$n.y/DryDays2$days * 100 
+   yearly_df<- DryDays2 %>%
+     dplyr::select(year, 'n.x') %>%
+     magrittr::set_colnames(c('Year', "Percent_Dry"))
+    
+    
+   
+    
+  
+   
+    
+  
 #Build Table with columns DOY, Q year 1, ..., Q year n
-  #Might make some analyses easier (Migh be easier to account for leap years)
+  #Might make some analyses easier (Might be easier to account for leap years)
 
-#Calculate frequency of No Flow
-  #Group by year
-  for (i in num years){
-    for (j in length of year){
-      if (discharge == 0){
-        drydays+=1
-      }
-      percent<-drydays/length [i]*100
-      }
-    }
 
 
 #Number of Discrete No Flow Periods
   #While loop through days to check for 3+ day periods of 0 flow
-
+  heat_df<-heatwaveR::exceedance(stream_df, x=Date, y=discharge_cms, 0.00001, below = TRUE, minDuration = 3)
+  dryPeriods<-(heat_df$exceedance) %>%
+    dplyr::select(exceedance_no, index_start, index_end, duration, date_start, date_end) %>%
+    magrittr::set_colnames(c("DryPeriod_No","index_start", "index_end", 
+                             "duration", "date_start", "date_end")) %>%
+    dplyr::mutate(DOY_start = lubridate::yday(date_start),
+                  DOY_end= lubridate::yday(date_end),
+                  year_start = lubridate::year(date_start),
+                  year_end = lubridate::year(date_end),
+                  year_dif= year_end - year_start,) %>%
+    dplyr::group_by(year_start)
+    
+ 
 #Average Length of No Flow
   #average from above
   #In order to plot over time, keep the year
@@ -46,3 +81,4 @@ data_path<- file.path("data", "Historical_Larned_Arkflow.CSV")
   #Determine First day of Year dry
   #Determine average first day over all years
 
+}
--- code/paths+packages.R
+++ code/paths+packages.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(dataRetrieval)
 #library(EcoHydRology) Doesn't agree with magrittr pipe, will load when needed
 library(lubridate)
+library(heatwaveR)
 
 ## color palettes
 # categorical color palette from https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
