3f0918b790c208a90495d6ac474781fd032bc779
kylecomp <kac15c@my.fsu.edu>
2020-07-15 13:56:02 -0500

    Update SPEI and Meteorologic calculations

--- code/Meteorological_02_Plotting.R
+++ code/Meteorological_02_Plotting.R
@@ -3,14 +3,22 @@
 source(file.path("code", "paths+packages.R"))
 
 data_path<- file.path("data", "Larned_Meteo.CSV")
-
 df_meteo<- readr::read_csv(data_path, col_types = cols())
+df_meteo <- df_meteo %>%
+  dplyr::slice(274:n())
 # set units
+
+
+data_path2<- file.path("data", "ArkLarned_DryPeriods.CSV")
+dryPeriods<- readr::read_csv(data_path2, col_types = cols())
+rects <- data.frame(start=dryPeriods$date_start, end=dryPeriods$date_end, group=seq_along(start))
 
 
 df_meteo$prcp <- NULL
 df_meteo$tmax <- NULL
 df_meteo$tmin <- NULL
+
+df_meteo$AvgTempC<-(df_meteo$tmax_c+ df_meteo$tmin_c)/2
 
 # summarize by year, month
 df_meteo$year <- lubridate::year(df_meteo$date)
@@ -26,14 +34,16 @@ df_meteo_mo <-
                    n_tmin = sum(is.finite(tmin_c)),
                    prcp_mm_sum = sum(prcp_mm, na.rm = T),
                    tmax_c_mean = mean(tmax_c, na.rm = T),
-                   tmin_c_mean = mean(tmin_c, na.rm = T)) %>% 
+                   tmin_c_mean = mean(tmin_c, na.rm = T), 
+                   TAvg_c_mean = mean(AvgTempC, na.rm = T)) %>%
   dplyr::mutate(n_days_mo = lubridate::days_in_month(month)) %>% 
   dplyr::ungroup()
+df_meteo_mo$date<-as.Date(with(df_meteo_mo, paste(year, month, 1,sep="-")), "%Y-%m-%d") 
 readr::write_csv(df_meteo_mo, file.path("data", "LArned_Meteo_Month.CSV"))
 
 
 #Assign Date Value for middle of month to plot summary stats
-df_meteo_mo$date <- paste(df_meteo_mo$year, df_meteo_mo$month, "15", sep="-") %>% ymd() %>% as.Date()
+df_meteo_mo$date <- paste(df_meteo_mo$year, df_meteo_mo$month, "1", sep="-") %>% ymd() %>% as.Date()
 
 # set months with too many missing days to NA
 day_thres <- 3
@@ -69,23 +79,44 @@ ggplot(data=df_meteo)+
     sec.axis = sec_axis( trans=~.*1, name="Precip (mm)")
     )
 
+ggplot(data=df_meteo_mo)+
+  geom_line(data=df_meteo_mo, aes(x=date, y= TAvg_c_mean))+
+  geom_rect(data=rects, inherit.aes=FALSE,
+            aes(xmin=start, xmax=end, ymin= min(df_meteo_mo$TAvg_c_mean),
+                ymax= max(df_meteo_mo$TAvg_c_mean), group=group), 
+            color="transparent", fill="orange", alpha=0.3)+
+  labs(x = "Date",
+       y = "monthly mean Temperature (C)")
+
 
 
 #########Read in  Hydrograph Data############
 path_hydro <- file.path("data", "ArkLarned_Baseflow.CSV")
 df_hydro<- readr::read_csv(path_hydro, col_types = cols())
 
+df_hydro_mo <- df_hydro %>%
+  dplyr::group_by(year, month) %>% 
+  dplyr::summarize(n_discahrge = sum(is.finite(discharge_cms)),
+                   n_bt = sum(is.finite(bt)),
+                   n_qft= sum(is.finite(qft)),
+                   discharge_sum = sum(discharge_cms, na.rm = T),
+                   bt_sum = sum(bt, na.rm = T),
+                   qft_sum = sum(qft, na.rm = T)) %>% 
+  dplyr::mutate(n_days_mo = lubridate::days_in_month(month)) %>% 
+  dplyr::ungroup()
 
-river<- df_hydro %>%
+df_hydro_mo$date<-as.Date(with(df_hydro_mo, paste(year, month, 1,sep="-")), "%Y-%m-%d")
+
+river<- df_hydro_mo %>%
 ggplot() +
-  geom_line(aes(x=Date, y= log10(discharge_cms+.01)))+
-  geom_line(aes(x=Date, y = log10(bt +.01)), color = col.cat.blu)+
+  geom_line(aes(x=date, y= log10(discharge_sum+.01)))+
+  geom_line(aes(x=date, y = log10(bt_sum +.01)), color = col.cat.blu)+
   geom_rect(data=rects, inherit.aes=FALSE,
             aes(xmin=start, xmax=end, ymin= min(log10(df_hydro$discharge_cms)),
             ymax= max(log10(df_hydro$discharge_cms)), group=group), 
             color="transparent", fill="orange", alpha=0.3)+
   labs(x = "Date",
-       y = "Log10 Discharge (cms)")
+       y = "Log10 Monthly Discharge (cms)")
 
  rain<-ggplot()+
    geom_line(data=df_meteo_mo, aes(x=date, y= prcp_mm_sum))+
--- code/Meteorological_03_SPEI.R
+++ code/Meteorological_03_SPEI.R
@@ -6,8 +6,21 @@ data_path<- file.path("data", "Larned_Meteo_Month.CSV")
 
 df_meteo_mo<- readr::read_csv(data_path, col_types = cols())
 
+data_path2<- file.path("data", "ArkLarned_DryPeriods.CSV")
+dryPeriods<- readr::read_csv(data_path2, col_types = cols())
+rects <- data.frame(start=dryPeriods$date_start, end=dryPeriods$date_end, group=seq_along(start))
+
+
+df_meteo_mo$date<-as.Date(with(df_meteo_mo, paste(year, month, 1,sep="-")), "%Y-%m-%d")
+
+
+df_meteo_mo$DOY<-lubridate::yday(df_meteo_mo$date)
+###Potential Solar radiation, with latitude in RADIANS.  PotRadiation in [kJ m-2 d-1]
+df_meteo_mo$PotRadiation<-EcoHydRology::PotentialSolar(lat=38.205*3.14159265/180, Jday=df_meteo_mo$DOY)
+
 ##Calculate PET First
-df_meteo_mo$PET<-SPEI::hargreaves(Tmin = df_meteo_mo$tmin_c_mean, Tmax = df_meteo_mo$tmax_c_mean, lat = 38.205 )
+df_meteo_mo$PET<-SPEI::hargreaves(Tmin = df_meteo_mo$tmin_c_mean, Tmax = df_meteo_mo$tmax_c_mean,
+                                  Ra = df_meteo_mo$PotRadiation * .001, lat = 38.205, Pre = df_meteo_mo$prcp_mm_sum, na.rm = FALSE )
 
 
 ###Water Balance
@@ -15,14 +28,99 @@ CWBAL<-df_meteo_mo$prcp_mm_sum - df_meteo_mo$PET
 
 
 ##Make SPEI classes for 3, 6, 9, 12 month intervals
-spei3<-SPEI::spei(CWBAL, 3)
+spei3<-SPEI::spei(df_meteo_mo$prcp_mm_sum - df_meteo_mo$PET, 3)
 spei6<-SPEI::spei(CWBAL, 6)
 spei9<-SPEI::spei(CWBAL, 9)
 spei12<-SPEI::spei(CWBAL, 12)
 
+par(mfrow=c(2,1)) 
+plot(spei3) 
+plot(spei12, xlab="SPEI")
+
+plot(SPEI::spei(ts(df_meteo_mo$prcp_mm_sum - df_meteo_mo$PET,freq=12,start=c(1998,1)),12))
+  plot(rects)
 ##Plot SPEI
 SPEI::plot.spei(spei3, main = "SPEI 3 month interval")
-  
-SPEI::plot.spei(spei6, main = "SPEI 6 month interval")
-SPEI::plot.spei(spei9, main = "SPEI 9 month interval")
-SPEI::plot.spei(spei12, main = "SPEI 12 month interval")
+
+DF3 <- zoo::fortify.zoo(spei3$fitted) %>%
+dplyr::select(-Index) %>% 
+  dplyr::mutate(Period = zoo::as.yearmon(paste(df_meteo_mo$year, df_meteo_mo$month), "%Y %m")) %>% 
+  na.omit() %>% 
+  dplyr::mutate(sign = ifelse(ET0_har >= 0, "pos", "neg"))
+
+SPEI3<-ggplot(DF3) +
+  geom_bar(aes(x = Period, y = ET0_har, col = sign, fill = sign),
+           show.legend = F, stat = "identity") +
+  scale_color_manual(values = c("pos" = "darkblue", "neg" = "red")) +
+  scale_fill_manual(values = c("pos"  = "darkblue", "neg" = "red")) +
+  scale_y_continuous(limits = c(-3, 3), 
+                     breaks = -3:3) +
+  ylab("SPEI") + ggtitle("3-Month SPEI") +
+  theme_bw() + theme(plot.title = element_text(hjust = 0.5))
+
+DryPeriod<-ggplot()+
+    geom_rect(data=rects, inherit.aes=FALSE,
+            aes(xmin=start, xmax=end, ymin=min(DF3$ET0_har),
+                ymax=max(DF3$ET0_har), group=group), 
+            color="transparent", fill="orange", alpha=0.3)+
+    lims(x=c(as.Date("1998-10-01"), as.Date("2019-12-31")))
+      
+Combine3 = SPEI3 / DryPeriod
+Combine3
+
+
+DF6 <- zoo::fortify.zoo(spei6$fitted) %>%
+  dplyr::select(-Index) %>% 
+  dplyr::mutate(Period = zoo::as.yearmon(paste(df_meteo_mo$year, df_meteo_mo$month), "%Y %m")) %>% 
+  na.omit() %>% 
+  dplyr::mutate(sign = ifelse(ET0_har >= 0, "pos", "neg"))
+
+SPEI6<-ggplot(DF6) +
+  geom_bar(aes(x = Period, y = ET0_har, col = sign, fill = sign),
+           show.legend = F, stat = "identity") +
+  scale_color_manual(values = c("pos" = "darkblue", "neg" = "red")) +
+  scale_fill_manual(values = c("pos"  = "darkblue", "neg" = "red")) +
+  scale_y_continuous(limits = c(-3, 3), 
+                     breaks = -3:3) +
+  ylab("SPEI") + ggtitle("6-Month SPEI") +
+  theme_bw() + theme(plot.title = element_text(hjust = 0.5))
+Combine6 = SPEI6 / DryPeriod
+Combine6
+
+
+DF9 <- zoo::fortify.zoo(spei9$fitted) %>%
+  dplyr::select(-Index) %>% 
+  dplyr::mutate(Period = zoo::as.yearmon(paste(df_meteo_mo$year, df_meteo_mo$month), "%Y %m")) %>% 
+  na.omit() %>% 
+  dplyr::mutate(sign = ifelse(ET0_har >= 0, "pos", "neg"))
+
+SPEI9<-ggplot(DF9) +
+  geom_bar(aes(x = Period, y = ET0_har, col = sign, fill = sign),
+           show.legend = F, stat = "identity") +
+  scale_color_manual(values = c("pos" = "darkblue", "neg" = "red")) +
+  scale_fill_manual(values = c("pos"  = "darkblue", "neg" = "red")) +
+  scale_y_continuous(limits = c(-3, 3), 
+                     breaks = -3:3) +
+  ylab("SPEI") + ggtitle("9-Month SPEI") +
+  theme_bw() + theme(plot.title = element_text(hjust = 0.5))
+Combine9 = SPEI9 / DryPeriod
+Combine9
+
+DF12 <- zoo::fortify.zoo(spei12$fitted) %>%
+  dplyr::select(-Index) %>% 
+  dplyr::mutate(Period = zoo::as.yearmon(paste(df_meteo_mo$year, df_meteo_mo$month), "%Y %m")) %>% 
+  na.omit() %>% 
+  dplyr::mutate(sign = ifelse(ET0_har >= 0, "pos", "neg"))
+
+SPEI12<-ggplot(DF12) +
+  geom_bar(aes(x = Period, y = ET0_har, col = sign, fill = sign),
+           show.legend = F, stat = "identity") +
+  scale_color_manual(values = c("pos" = "darkblue", "neg" = "red")) +
+  scale_fill_manual(values = c("pos"  = "darkblue", "neg" = "red")) +
+  scale_y_continuous(limits = c(-3, 3), 
+                     breaks = -3:3) +
+  ylab("SPEI") + ggtitle("12-Month SPEI") +
+  theme_bw() + theme(plot.title = element_text(hjust = 0.5))
+Combine12 = SPEI12 / DryPeriod
+Combine12
+
